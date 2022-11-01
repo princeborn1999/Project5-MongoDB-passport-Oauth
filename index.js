56547661d068739d3f99e5bbbae9081a5a501ffd
Prince <princeborn1999@gmail.com>
2022-11-01 08:53:03 +0800

    [+]Homepage Setup

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hi");
+  res.render("index");
 });
 
 app.listen(8080, () => {
